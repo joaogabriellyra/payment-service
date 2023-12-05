c0e11a21e3783ded71d5be1476f336482faad7c6
joaogabriellyra <jgabriellyra@hotmail.com>
2023-12-05 15:47:44 -0300

    encriptografando a senha com bcrypt

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from "express";
+import IUser from "../interfaces/IUser";
 import UserService from "../service/UserService";
+import bcrypt from 'bcrypt';
 
 export default class UserController {
     async newUser(req: Request, res: Response) {
-        const { firstName, lastName, email, password } = req.body;
-        const newUser = await new UserService().newUser(firstName, lastName, email, password);
+        const { firstName, lastName, email, password }: IUser = req.body;
+        const newPassword: string = await bcrypt.hash(password, 10);
+        const newUser = await new UserService().newUser(firstName, lastName, email, newPassword);
         return res.status(201).json(newUser);
     }
 }
