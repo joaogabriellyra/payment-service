907f2f79586e306e04b0137ecbe9c0199a52342f
joaogabriellyra <jgabriellyra@hotmail.com>
2023-12-05 15:57:33 -0300

    adicionado tipo da constante que recebe o usuário cadastrado

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -3,12 +3,13 @@ import IUser from "../interfaces/IUser";
 import UserService from "../service/UserService";
 import bcrypt from 'bcrypt';
 import { HttpCodes } from "../utils/httpCodes";
+import { User } from "../database/entity/User";
 
 export default class UserController {
     async newUser(req: Request, res: Response) {
         const { firstName, lastName, email, password }: IUser = req.body;
         const newPassword: string = await bcrypt.hash(password, 10);
-        const newUser = await new UserService().newUser(firstName, lastName, email, newPassword);
+        const newUser: User = await new UserService().newUser(firstName, lastName, email, newPassword);
         return res.status(HttpCodes.CREATED).json(newUser);
     }
 }
