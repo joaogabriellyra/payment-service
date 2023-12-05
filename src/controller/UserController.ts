fb7f01f834c72eb8a5f816f0cc9e7e0c1e48bdfb
joaogabriellyra <jgabriellyra@hotmail.com>
2023-12-05 15:55:46 -0300

    rota que cria usuário já informa o código http a partir do enum HttpCodes

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -2,12 +2,13 @@ import { Request, Response } from "express";
 import IUser from "../interfaces/IUser";
 import UserService from "../service/UserService";
 import bcrypt from 'bcrypt';
+import { HttpCodes } from "../utils/httpCodes";
 
 export default class UserController {
     async newUser(req: Request, res: Response) {
         const { firstName, lastName, email, password }: IUser = req.body;
         const newPassword: string = await bcrypt.hash(password, 10);
         const newUser = await new UserService().newUser(firstName, lastName, email, newPassword);
-        return res.status(201).json(newUser);
+        return res.status(HttpCodes.CREATED).json(newUser);
     }
 }
