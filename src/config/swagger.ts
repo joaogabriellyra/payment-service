37bc4160f5acc91d7a4c7c49d91489344c6d1fd9
joaogabriellyra <jgabriellyra@hotmail.com>
2023-12-12 14:43:27 -0300

    adicionada a configuração do swagger

---
+++ src/config/swagger.ts
@@ -0,0 +1,29 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+
+export const swaggerConfig: swaggerJSDoc.OAS3Options = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Payment Service',
+      description: 'Documentação da API do projeto Payment Service.',
+      version: '1.0.0'
+    },
+    host: 'localhost:3001',
+    // Não obrigatório, serve apenas para definir a ordem das categorias
+    tags: [],
+    externalDocs: {
+      description: 'View swagger.json',
+      url: '../swagger.json'
+    },
+    components: {
+      securitySchemes: {
+        BearerAuth: {
+          in: 'header',
+          type: 'http',
+          scheme: 'bearer'
+        }
+      }
+    }
+  },
+  apis: ['src/controllers/*.ts', 'controllers/*.js']
+};
