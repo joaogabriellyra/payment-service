ad8a12059b6cf9b4efc5192344af64de353cf411
joaogabriellyra <jgabriellyra@hotmail.com>
2023-12-13 02:43:25 -0300

    O passo a passo da aplicação foi adicionado ao Swagger

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -5,7 +5,29 @@ export const swaggerConfig: swaggerJSDoc.OAS3Options = {
     openapi: '3.0.0',
     info: {
       title: 'Payment Service',
-      description: 'Documentação da API do projeto Payment Service.',
+      description: `O usuário poderá criar uma conta no sistema em que será permitido realizar e receber transferências
+        entre os usuários já cadastrados. O usuário também poderá alterar as suas informações. 
+        Será permitido um cadastro por CPF.
+        O passo a passo da construção da aplicação consiste nas seguintes etapas:
+        \n- criar um docker-compose e criar uma rota /healthy que retorna ‘the application is healthy’ <b>OK</b>
+        \n- configurar typeORM <b>OK</b>
+        \n- a documentação do projeto será pelo swagger 
+        \n- cadastrar usuário com post 
+        \n- as senhas serão encriptografadas ao serem salvas 
+        \n- haverá um e-mail para confirmação de cadastro 
+        \n- fazer post para login
+        \n- a autenticação do login será via JWT 
+        \n- pegar informações do usuário com get 
+        \n- atualizar informações do usuário com put 
+        \n- deletar usuário com delete (deleção lógica) 
+        \n- fazer post depositando 
+        \n- fazer get saldo 
+        \n- fazer post transferindo (todo usuário cadastrado poderá receber uma transferência) 
+        \n- fazer post sacando 
+      `,
+      contact: {
+        email: 'jgabriellyra@hotmail.com'
+      },
       version: '1.0.0'
     },
     host: 'localhost:3001',
