c997ada71440665e0739cb165476e87ced191a91
joaogabriellyra <jgabriellyra@hotmail.com>
2024-01-04 18:36:03 -0300

    usuário é adicionado no banco de dados com a senha criptografada e é retornado os dados públicos do cliente

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -3,6 +3,7 @@ import IUser from "../interfaces/IUser";
 import UserService from "../service/UserService";
 import { HttpCodes } from "../utils/httpCodes";
 import { User } from "../database/entity/User";
+import { hash } from "bcrypt";
 
 export default class UserController {
     async newUser(req: Request, res: Response) {
@@ -12,8 +13,9 @@ export default class UserController {
             if (user) {
                 return res.status(HttpCodes.BAD_REQUEST).json({ message: "User already exists" })
             } 
-            const newUser: User = await new UserService().newUser(firstName, lastName, email, password);
-            return res.status(HttpCodes.CREATED).json(newUser);
+            const newPassword = await hash(password, 10);
+            const { id, createdAt }: User = await new UserService().newUser(firstName, lastName, email, newPassword);
+            return res.status(HttpCodes.CREATED).json({ newUser: { email, firstName, lastName, id, createdAt } });
         } catch (error) {
             return res.status(HttpCodes.BAD_REQUEST).json({ message: error.message })
         }
