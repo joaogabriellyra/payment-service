e539c81334715a4883fc224c3e01fd94fdb48842
joaogabriellyra <jgabriellyra@hotmail.com>
2024-01-08 15:55:00 -0300

    dados que o usuário utilizou ao cadastrar estão sendo validados via validator express

--- src/router/user.route.ts
+++ src/router/user.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import UserController from "../controller/UserController";
+import { handleError } from "../middlewares/validation.result";
+import { userFields } from "../middlewares/user.fields";
 
 const router = Router();
 
-router.post('/users/new-user', new UserController().newUser);
+router.post('/users/new-user', userFields(), handleError, new UserController().newUser);
 
 export default router;
