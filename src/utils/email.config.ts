2ac1997f7f9f38e68c512908f6336f8bc4af7fd9
joaogabriellyra <jgabriellyra@hotmail.com>
2024-01-17 15:06:23 -0300

    o e-mail de confirmação de cadastro agora envia o token para fins de verificação

--- src/utils/email.config.ts
+++ src/utils/email.config.ts
@@ -13,12 +13,12 @@ const transporter = createTransport({
       }
 })
 
-export const mail = (userEmail, userName) => {
+export const mail = (userEmail: string, userName: string, token: string) => {
     transporter.sendMail({
         from: process.env.MAIL_USERNAME,
         to: userEmail,
         subject: 'Confirmação seu e-mail de cadastro',
-        html: `<h2>Olá, ${userName}!</h2><p>Para a sua segurança, confirme se este é o endereço de e-mail que você cadastrou no Payment Service.</p>`
+        html: `<h2>Olá, ${userName}!</h2><p>Para a sua segurança, confirme se este é o endereço de e-mail que você cadastrou no Payment Service.</p><p>Clique no link abaixo</p><a href="localhost:3002/confirm-email/${token}></a>" `
     })
 }
 
