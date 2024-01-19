9e62e4c4833975807a15ebd92a00762fd094cc43
joaogabriellyra <jgabriellyra@hotmail.com>
2024-01-18 21:17:13 -0300

    fluxo de confirmação de e-mail via swagger está ocorrendo de forma correta

--- src/middlewares/remove.token.ts
+++ src/middlewares/remove.token.ts
@@ -1,8 +1,8 @@
-import { header } from "express-validator"
+import { query } from "express-validator"
 import TokenService from "../service/TokenService"
 
 export const removeToken = () => (
-    [header('authorization').isJWT().custom(async value => {
+    [query('authorization').isJWT().custom(async value => {
         const { token } = await new TokenService().getToken(value);
         if (!token) {
             throw new Error();
